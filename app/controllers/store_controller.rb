c11a9501960972f9468c0e36c9f12fe8edc792eb
lhptattoo <lucass.hauptmann@gmail.com>
2018-02-09 13:36:41 -0200

    Task D completed

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,18 @@
-class StoreController < ApplicationController
+ class StoreController < ApplicationController
+
+  def increment_count
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+    session[:counter] += 1
+    end
+  end
+
   def index
     t = Time.now
     @products = Product.order(:title)
     @actualdate = t.strftime('%v')
     @actualtime = t.strftime('%r')
+    increment_count
   end
 end
